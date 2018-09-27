154a6b615d87d5ebebdb5a1dc4c1ab89a4a32808
Brian Cappello <briancappello@gmail.com>
2018-09-26 19:29:52 -0600

    release v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flask-sqlalchemy-unchained',
-    version='0.1.0',
+    version='0.1.1',
     description='Integrates SQLAlchemy Unchained with Flask',
     author='Brian Cappello',
     license='MIT',
